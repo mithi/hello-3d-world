41dc5b34c713ca1640197af573a2ca4f2c060f39
Mithi <mithi.sevilla@gmail.com>
2020-08-29 20:24:36 +0800

    feat: Update slider css

--- src/input-components/slider/Slider.tsx
+++ src/input-components/slider/Slider.tsx
@@ -4,6 +4,7 @@ import { sliderCss, sliderContainerCss } from "./SliderCss"
 
 const Slider = () => (
     <div css={sliderContainerCss}>
+        <label>My label</label> : <span>70</span>
         <input css={sliderCss} type="range" min="0" max="100" value="70" />
     </div>
 )
--- src/input-components/slider/SliderCss.tsx
+++ src/input-components/slider/SliderCss.tsx
@@ -5,25 +5,50 @@ const sliderCss = css`
     -webkit-appearance: none;
     -moz-appearance: none;
     appearance: none;
+    background-color: #e91e63;
     width: 100%;
-    height: 0.25rem;
+    height: 0.5rem;
     outline: none;
     border-radius: 0.25rem;
+    &:hover,
+    &:active {
+        background: #673ab7;
+    }
+    &:hover&::-webkit-slider-thumb {
+        border: 0.25rem solid #673ab7;
+    }
+    &:hover&::-moz-range-thumb {
+        border: 0.25rem solid #673ab7;
+    }
     &::-webkit-slider-thumb {
         -webkit-appearance: none;
         appearance: none;
         width: 1.5rem;
         height: 1.5rem;
-        background-color: #f44336;
-        border: 0.25rem solid #fff;
+        background-color: black;
+        border: 0.25rem solid #e91e63;
         border-radius: 1rem;
         cursor: pointer;
+    }
+    &::-moz-range-thumb {
+        background: black;
+        width: 1.5rem;
+        height: 1.5rem;
+        border-radius: 1rem;
+        cursor: pointer;
+        border: 0.25rem solid #e91e63;
     }
 `
 
 const sliderContainerCss = css`
     padding: 10px;
-    background-color: #f44336;
+    background-color: black;
+    font-size: 15px;
+    font-family: monaco;
+    color: #e91e63;
+    &:hover {
+        color: #673ab7;
+    }
 `
 
 export { sliderCss, sliderContainerCss }
