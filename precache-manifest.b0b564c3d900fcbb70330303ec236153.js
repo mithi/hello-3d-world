ff71a0e01b8d24d691f6668f5778704d14ada8b1
Mithi <mithi.sevilla@gmail.com>
2020-08-31 02:13:15 +0800

    Updates

---
+++ precache-manifest.b0b564c3d900fcbb70330303ec236153.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e5fd191fe79bf1262538e09912244fd",
+    "url": "/hello-3d-world/index.html"
+  },
+  {
+    "revision": "2bf558711d7951839ffc",
+    "url": "/hello-3d-world/static/css/main.123b2b1b.chunk.css"
+  },
+  {
+    "revision": "ec57a26c2b17898f1200",
+    "url": "/hello-3d-world/static/js/2.0ed69066.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hello-3d-world/static/js/2.0ed69066.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bf558711d7951839ffc",
+    "url": "/hello-3d-world/static/js/main.c3f04c09.chunk.js"
+  },
+  {
+    "revision": "5cb2843c933fae97e1a4",
+    "url": "/hello-3d-world/static/js/runtime-main.b1802b7a.js"
+  }
+]);
